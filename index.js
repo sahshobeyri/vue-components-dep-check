dea8c9143ac3ff3b66cd98e406ccc303ae208fa6
sahshobeyri <sahshobeyri@gmail.com>
2022-03-10 15:29:24 +0330

    feat: promisify stuff

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const glob = require('glob')
 const path = require('path')
 const { parseComponent } = require('vue-sfc-parser')
-const vueFiles = []
 const fs = require('fs')
+const fsPromises = fs.promises;
 
 const PROJ_DIR = "D:/Projects/Basalam/basalam-nuxt"
-const importVueComponentRegex = /(import|require).*/gm
+const importVueComponentRegex = /(import|require)(.*)(from)(.*)/gm
+
+const vueFiles = []
 
 glob(path.join(PROJ_DIR,"/**/*.vue"), {}, function (er, files) {
+    let promises = []
     files.slice(0,1).forEach(f => {
         console.log(path.relative(PROJ_DIR,f))
-        fs.readFile(f, function(err, data) {
+        const pms = fsPromises.readFile(f).then(data => {
             const scriptPart = parseComponent(data.toString()).script.content;
             const regexResult = scriptPart.matchAll(importVueComponentRegex);
-            Array.from(regexResult).forEach(i => console.log(i[0]));
+            let imports = []
+            Array.from(regexResult).forEach(i => {
+                imports.push(i[0])
+            });
+            vueFiles.push({importer: f, imports})
         });
+        promises.push(pms)
     })
+    Promise.allSettled(promises).then(() => console.log(vueFiles))
     console.log('total count: ',files.length)
 })
