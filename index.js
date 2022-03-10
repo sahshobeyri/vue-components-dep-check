9c362de5f7dcc971b664d2f9d940ac964aedb37a
sahshobeyri <sahshobeyri@gmail.com>
2022-03-10 15:32:41 +0330

    style: reformat indents

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const glob = require('glob')
 const path = require('path')
-const { parseComponent } = require('vue-sfc-parser')
+const {parseComponent} = require('vue-sfc-parser')
 const fs = require('fs')
 const fsPromises = fs.promises;
 
@@ -9,21 +9,21 @@ const importVueComponentRegex = /(import|require)(.*)(from)(.*)/gm
 
 const vueFiles = []
 
-glob(path.join(PROJ_DIR,"/**/*.vue"), {}, function (er, files) {
-    let promises = []
-    files.slice(0,1).forEach(f => {
-        console.log(path.relative(PROJ_DIR,f))
-        const pms = fsPromises.readFile(f).then(data => {
-            const scriptPart = parseComponent(data.toString()).script.content;
-            const regexResult = scriptPart.matchAll(importVueComponentRegex);
-            let imports = []
-            Array.from(regexResult).forEach(i => {
-                imports.push(i[0])
-            });
-            vueFiles.push({importer: f, imports})
-        });
-        promises.push(pms)
-    })
-    Promise.allSettled(promises).then(() => console.log(vueFiles))
-    console.log('total count: ',files.length)
+glob(path.join(PROJ_DIR, "/**/*.vue"), {}, function (er, files) {
+  let promises = []
+  files.slice(0, 1).forEach(f => {
+    console.log(path.relative(PROJ_DIR, f))
+    const pms = fsPromises.readFile(f).then(data => {
+      const scriptPart = parseComponent(data.toString()).script.content;
+      const regexResult = scriptPart.matchAll(importVueComponentRegex);
+      let imports = []
+      Array.from(regexResult).forEach(i => {
+        imports.push(i[0])
+      });
+      vueFiles.push({importer: f, imports})
+    });
+    promises.push(pms)
+  })
+  Promise.allSettled(promises).then(() => console.log(vueFiles))
+  console.log('total count: ', files.length)
 })
