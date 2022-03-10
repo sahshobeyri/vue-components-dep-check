0653710a3112295d700f3903dd695149e1cd9759
sahshobeyri <sahshobeyri@gmail.com>
2022-03-10 15:15:03 +0330

    feat: find import lines

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const vueFiles = []
 const fs = require('fs')
 
 const PROJ_DIR = "D:/Projects/Basalam/basalam-nuxt"
+const importVueComponentRegex = /(import|require).*/gm
 
 glob(path.join(PROJ_DIR,"/**/*.vue"), {}, function (er, files) {
     files.slice(0,1).forEach(f => {
         console.log(path.relative(PROJ_DIR,f))
         fs.readFile(f, function(err, data) {
-            console.log(parseComponent(data.toString()).script.content)
+            const scriptPart = parseComponent(data.toString()).script.content;
+            const regexResult = scriptPart.matchAll(importVueComponentRegex);
+            Array.from(regexResult).forEach(i => console.log(i[0]));
         });
     })
     console.log('total count: ',files.length)
