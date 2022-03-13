44a0578f04e487dbec145e4c01876d1ca5f6988c
sahshobeyri <sahshobeyri@gmail.com>
2022-03-13 22:17:02 +0330

    WIP: index.js is moving forward using modules!

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const {restoreVueExtensionInPath} = require("./src/path-util");
+const {restoreIndexFileInPath} = require("./src/path-util");
 const {doReport} = require('./src/reporter')
 const {pathFromProjDir} = require('./src/path-util')
 const {extractFileImports} = require('./src/script-parser')
@@ -8,7 +10,9 @@ async function main () {
   const allVueFiles = await readAllVueFiles(PROJ_DIR)
   const allCompsWithImports = allVueFiles.map(({filePath,fileStr}) => ({
     component:  pathFromProjDir(filePath),
-    // imports: extractFileImports(fileStr,filePath)
+    imports: extractFileImports(fileStr,filePath)
+      .map(restoreIndexFileInPath)
+      .map(restoreVueExtensionInPath)
   }));
   console.log(allCompsWithImports)
   doReport(allCompsWithImports)
