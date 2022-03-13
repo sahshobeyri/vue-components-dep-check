0ca291e0fd282ecbb8838da17eba66eface85262
sahshobeyri <sahshobeyri@gmail.com>
2022-03-14 01:59:28 +0330

    refactor: remove unused stuff in reporter.js

--- src/reporter.js
+++ src/reporter.js
@@ -1,20 +1,5 @@
 const {createDirIfNotExist, writeFile} = require('./fs-util')
 const {datetimeStrForFilename} = require('./string-util')
-const {findFilesWithNoWayToEntries, findUnusedFiles} = require('./analyzer')
-
-function reportFilesWithNoWayToEntry(vueFiles) {
-  const result = findFilesWithNoWayToEntries(vueFiles)
-  doReport(result)
-}
-
-function reportUnusedFiles(vueFiles) {
-  const result = findUnusedFiles(vueFiles)
-  doReport(result)
-}
-
-function reportVueFiles(vueFiles) {
-  doReport(vueFiles)
-}
 
 function doReport(data) {
   createDirIfNotExist('reports/')
