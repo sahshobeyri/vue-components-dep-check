6738b61b10ba44ec0103e31b239d1bdd1440bb71
sahshobeyri <sahshobeyri@gmail.com>
2022-03-13 17:41:31 +0330

    refactor: extract config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+export const EXCLUDED_FOLDERS = ['node_modules']
+export const PAGES_FOLDER = 'pages'
+export const LAYOUTS_FOLDER = 'layouts'
+export const PROJ_DIR = 'G:/REAL DRIVE D/Projects/Basalaam/_DoorKari/BasalamNuxtNew/basalam-nuxt'
