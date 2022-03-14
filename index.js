408715819df6764f1c84ea9fec9bbc6a895451eb
sahshobeyri <sahshobeyri@gmail.com>
2022-03-14 15:15:40 +0330

    feat: add more informative and structured 'console.log's

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const {readAllVueFiles} = require('./src/glob-util')
 const {PROJ_DIR} = require('./src/config')
 
 async function main() {
+  console.group('Reading .vue files...')
   const allVueFiles = await readAllVueFiles(PROJ_DIR)
+  console.log('Read ' + allVueFiles.length + ' files.')
+  console.groupEnd()
+
+  console.group('Extracting imports from .vue files...')
   const allCompsWithImports = allVueFiles.map(({filePath, fileStr}) => ({
     component: pathFromProjDir(filePath),
     imports: extractFileImports(fileStr, filePath)
@@ -24,17 +29,26 @@ async function main() {
       .filter(nonVueFilesFilter)
       .map(pathFromProjDir)
   }));
+  console.log('Done.')
+  console.groupEnd()
 
+
+  console.group('Creating Usage-Graph...')
   const usageGraph = createUsageGraph(allCompsWithImports)
-
+  console.log('Usage-Graph created.')
   console.log('Number of nodes', usageGraph.order);
   console.log('Number of edges', usageGraph.size);
+  console.groupEnd()
 
+  console.group('Finding orphan components...')
   const orphans = findOrphans(usageGraph)
   doReport(orphans)
+  console.groupEnd()
 
+  console.group('Converting graph To gexf format...')
   const gexfString = convertGraphToGexf(usageGraph)
   doReport(gexfString, {isRaw: true, ext: "gexf"})
+  console.groupEnd()
 
 }
 
