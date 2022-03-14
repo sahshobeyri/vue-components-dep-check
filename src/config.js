1f6f169297ec35b5432ff14fc50603d97bfc076c
sahshobeyri <sahshobeyri@gmail.com>
2022-03-14 14:59:27 +0330

    feat: add ENTRY_VIRTUAL_NODE to config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 const EXCLUDED_FOLDERS = ['node_modules']
 const PAGES_FOLDER = 'pages'
 const LAYOUTS_FOLDER = 'layouts'
-const PROJ_DIR = 'G:/REAL DRIVE D/Projects/Basalaam/_DoorKari/BasalamNuxtNew/basalam-nuxt'
+const PROJ_DIR = 'D:/Projects/Basalam/basalam-nuxt'
+const ENTRY_VIRTUAL_NODE = '---ENTRY-VIRTUAL-NODE---'
 
 module.exports = {
   EXCLUDED_FOLDERS,
   PAGES_FOLDER,
   LAYOUTS_FOLDER,
   PROJ_DIR,
+  ENTRY_VIRTUAL_NODE,
 }
